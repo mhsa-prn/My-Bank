5b731f97a9d7ae1fdf13e0f12ef3d7ad1bc69bd2
Mahsa Piran <piran_mahsa@yahoo.com>
2024-10-28 12:52:34 +0330

    Define default accounts

---
+++ script.js
@@ -0,0 +1,30 @@
+//----------------------- Define accounts -----------------------
+let account1 = {
+    owner: 'Mahsa Piran',
+    pin: 1111,
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.5,
+};
+
+let account2 = {
+    owner: 'Jessica Davis',
+    pin: 2222,
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.2,
+};
+
+let account3 = {
+    owner: 'Steven Thomas Williams',
+    pin: 3333,
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    interestRate: 0.7,
+};
+
+let account4 = {
+    owner: 'Sarah Smith',
+    pin: 4444,
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+};
+
+let accounts = [account1, account2, account3, account4];
